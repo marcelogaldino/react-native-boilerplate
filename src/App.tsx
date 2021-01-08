29fff3a42c80c77393069a9d461436d359aa3f4d
marcelog <marcelog@EQUIP2618>
2021-01-08 15:33:35 -0300

    fix: eslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { View, StatusBar, Text } from 'react-native';
 const App: React.FC = () => (
   <>
     <StatusBar backgroundColor="#bb1223" barStyle="light-content" />
-    <View style={{
-      flex: 1, backgroundColor: '#bb1223', alignItems: 'center', justifyContent: 'center',
-    }}
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#bb1223',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
     >
-      <Text style={{ color: 'white', fontSize: 28 }}>React Native Boilerplate</Text>
-      <Text style={{ color: 'white', fontSize: 18, fontStyle: 'italic' }}>Developed by: Marcelo Galdino</Text>
+      <Text style={{ color: 'white', fontSize: 28 }}>
+        React Native Boilerplate
+      </Text>
+      <Text style={{ color: 'white', fontSize: 18, fontStyle: 'italic' }}>
+        Developed by: Marcelo Galdino
+      </Text>
     </View>
   </>
 );
